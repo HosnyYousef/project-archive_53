0ebb12ce3b1c6fce67b2a5edae5e3285d2c1e6ac
hosnyyousef <yousefhosny.hm@gmail.com>
2025-06-04 15:51:41 +0300

    running it

--- config/passport.js
+++ config/passport.js
@@ -4,8 +4,8 @@ const User = require("../models/User");
 
 module.exports = function (passport) {
   passport.use(
-    new LocalStrategy({ usernameField: "email" }, (email, password, done) => {
-      User.findOne({ email: email.toLowerCase() }, (err, user) => {
+    new LocalStrategy({ usernameField: "email" }, async (email, password, done) => {
+      const user = await User.findOne({ email: email.toLowerCase() }, (err, user) => {
         if (err) {
           return done(err);
         }
