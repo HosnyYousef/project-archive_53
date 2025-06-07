1b582e087d22e99d3934654dc84584c216d3307c
hosnyyousef <yousefhosny.hm@gmail.com>
2025-06-07 11:38:25 +0300

    running code prep

--- controllers/posts.js
+++ controllers/posts.js
@@ -59,28 +59,18 @@ module.exports = {
       console.log(err);
     }
   },
-
-deletePost: async (req, res) => {
-  try {
-    // Find post by id
-    let post = await Post.findById(req.params.id);
-
-    if (!post) {
-      console.log("Post not found");
-      return res.redirect("/profile");
+  deletePost: async (req, res) => {
+    try {
+      // Find post by id
+      let post = await Post.findById({ _id: req.params.id });
+      // Delete image from cloudinary
+      await cloudinary.uploader.destroy(post.cloudinaryId);
+      // Delete post from db
+      await Post.remove({ _id: req.params.id });
+      console.log("Deleted Post");
+      res.redirect("/profile");
+    } catch (err) {
+      res.redirect("/profile");
     }
-
-    // Delete image from cloudinary
-    await cloudinary.uploader.destroy(post.cloudinaryId);
-
-    // Delete post from db (modern method)
-    await Post.deleteOne({ _id: req.params.id });
-
-    console.log("Deleted Post");
-    res.redirect("/profile");
-  } catch (err) {
-    console.error("Delete error:", err);
-    res.redirect("/profile");
-  }
-},
+  },
 };
